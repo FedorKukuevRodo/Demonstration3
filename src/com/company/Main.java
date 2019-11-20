887e5059a584b83179199228e295864a431bfdcb
FedorKukuevRodo <57758521+FedorKukuevRodo@users.noreply.github.com>
2019-11-20 16:53:27 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) {
-        String ChromeDriverDirectory = System.getProperty("user.dir") + "/webdriver/ChromeDriver/chromedriver.exe";
+        String ChromeDriverDirectory = System.getProperty("user.dir") + "/webdriver/ChromeDriver/chromedriver";
         System.setProperty("webdriver.chrome.driver", ChromeDriverDirectory);
         WebDriver myDriver = new ChromeDriver();
         myDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
