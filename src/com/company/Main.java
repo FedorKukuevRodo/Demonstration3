4d3a58bce2eb71141da3a334a734430bc1753f99
FedorKukuevRodo <57758521+FedorKukuevRodo@users.noreply.github.com>
2019-11-26 19:03:03 +0300

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+ppackage com.company;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,20 +6,30 @@ import org.openqa.selenium.interactions.Actions;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static String rodoUrl;
+    private static String Env;
+
     public static void main(String[] args) {
         //find chrome driver on your machine, only one of them should be uncommented
         //FOR MAC:
-        System.setProperty("webdriver.chrome.driver", "/Users/jillduhl/Desktop/webdriver/ChromeDriver/chromedriver");
+        //System.setProperty("webdriver.chrome.driver", "/Users/jillduhl/Desktop/webdriver/ChromeDriver/chromedriver");
         //FOR WINDOWS:
-        //System.setProperty("webdriver.chrome.driver", "D:\\webDriver\\webdriver\\ChromeDriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\webDriver\\webdriver\\ChromeDriver\\chromedriver.exe");
+        //Testing environment, only one of them should be uncommented
+        Env = "prod"; // Testing on prod
+        //Env = "dev"; // Testing on dev
 
         WebDriver myDriver = new ChromeDriver();
         myDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //if it can't find an element on the
         // page right away it does not stop the script immediately, it keeps trying for 10sec
-        myDriver.get("https://www.rodo.com");   //open rodo.com in chrome browser
-        myDriver.manage().window().maximize();   //open browser to fullscreen
-        String email = "seltest4+1122@testmail.com"; //email that's going to be used for registration and login
-        String password = "374502Qq!";           //password
+        if (Env == "prod") {
+            rodoUrl = "https://www.rodo.com";
+        } else {rodoUrl = "https://web.dev.rodo.com";}
+        myDriver.get(rodoUrl);                                                          //open rodo.com in chrome browser
+        myDriver.manage().window().maximize();                                          //open browser to fullscreen
+        String email = "fedor.kukuev+1126test99@test.com";                              //email that's going to be used for registration and login
+        //String email = "jduhl+test" + (int)(Math.random() * 99999 + 1) + "@rodo.com"; //in case if you want email to be random
+        String password = "374502Qq!";                                                  //password
         //Here the script starts to work
         registration(myDriver, email, password);                    //calling registration function
         logout(myDriver);                                           //calling logout function
@@ -29,18 +39,20 @@ public class Main {
         myDriver.close();                                           //close the browser
     }
     private static void login(WebDriver myDriver, String email, String password) {
-        myDriver.get("https://www.rodo.com/login");                                     //open rodo.com/login
+        myDriver.get(rodoUrl + "/login");                                               //open rodo.com/login
         myDriver.findElement(By.xpath("//input[@name='email']")).sendKeys(email);       //type email in the input field
         myDriver.findElement(By.xpath("//input[@name='password']")).sendKeys(password); //type password
         myDriver.findElement(By.xpath("//button[@type='submit']")).click();             //click login
         System.out.println("login test completed");                                                  //write in console: test completed
     }
     private static void registration(WebDriver myDriver, String email, String password) {
-        myDriver.get("https://www.rodo.com/sign-up");                                                   //open rodo.com/signup
-        myDriver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("Jonathan");   //enter Jonathan in the input field
-        myDriver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Consumer");    //enter Consumer
-        myDriver.findElement(By.xpath("//input[@id='street']")).sendKeys("Testing street");//enter street address
-        myDriver.findElement(By.xpath("//input[@id='zip']")).sendKeys("11354 ");           //enter zip
+        String firstName = Env == "prod" ? "Jonathan" : "Lance";                         //first name: Jonathan for prod, Lance for dev
+        String lastName = Env == "prod" ? "Consumer" : "Syed";                           //last name: Consumer for prod, Syed for dev
+        myDriver.get(rodoUrl + "/sign-up");                                              //open rodo.com/signup
+        myDriver.findElement(By.xpath("//input[@id='firstName']")).sendKeys(firstName);  //enter first name in the input field
+        myDriver.findElement(By.xpath("//input[@id='lastName']")).sendKeys(lastName);    //enter last name in the input field
+        myDriver.findElement(By.xpath("//input[@id='street']")).sendKeys("1087 CRAIGVILLE RD");//enter street address
+        myDriver.findElement(By.xpath("//input[@id='zip']")).sendKeys("10918 ");           //enter zip
         myDriver.findElement(By.xpath("//input[@id='phone']")).sendKeys("7862001435");     //inter phone number
         myDriver.findElement(By.cssSelector("#dob")).click();                                           //click on DOB input field
         myDriver.findElement(By.xpath("//*[@id='__next']/div/div/div/section/div[3]/form/div[4]/div/div[2]/div/div[2]/div/div/div[2]/div[2]/div[2]/div[1]")).click(); // click on 6th of January
@@ -60,11 +72,12 @@ public class Main {
     }
 
     private static void freeSearchAZ(WebDriver myDriver) {
-        String[] makeList = { "Acura", "ALFA ROMEO", "Audi", "BMW", "Buick", "Cadillac", "Chevrolet",
-                             "Chrysler", "Dodge", "Fiat", "Ford", "Genesis", "GMC", "Honda",
-                             "Hyundai", "Infiniti", "Jaguar", "Jeep", "Land Rover", "Lexus", "Mazda",
-                             "Mercedes", "Mini", "Mitsubishi", "Porsche", "RAM", "Subaru", "Toyota",
-                             "Volkswagen", "Volvo", "Maserati"};            //list of makes
+        String[] makeList = {
+                "Acura", "ALFA ROMEO", "Audi", "BMW", "Buick", "Cadillac", "Chevrolet",
+                "Chrysler", "Dodge", "Fiat", "Ford", "Genesis", "GMC", "Honda",
+                "Hyundai", "Infiniti", "Jaguar", "Jeep", "Land Rover", "Lexus", "Mazda",
+                "Mercedes", "Mini", "Mitsubishi", "Porsche", "RAM", "Subaru", "Toyota",
+                "Volkswagen", "Volvo","Maserati"};            //list of makes
         String searchResults;                                                        //how many cars of this make found
         for (String make: makeList)                 //starting the cycle
         {
@@ -72,8 +85,7 @@ public class Main {
             myDriver.findElement(By.xpath("//button[@class='search-btn']")).click(); //clicking on the search icon
             searchResults = myDriver.findElement(By.xpath("//*[@id='__next']/div/div/div/div/div[2]/div[1]/strong[3]")).getText();  //getting the number of search results
             System.out.println(make + ": " + searchResults + " results");   //show the number of search results in console
-            myDriver.get("https://www.rodo.com");           //back to home page
+            myDriver.get(rodoUrl);                          //back to home page
         }                                                   //jump in the beginning of cycle with the next make
         System.out.println("freeSearchAZ test completed");  //write in console: test completed
     }
-}
